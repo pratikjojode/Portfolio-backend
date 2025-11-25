957005560fabad9ff917cfb0d7cc261ac7fc9e1b
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-11-25 20:47:35 +0530

    fixes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -15,23 +15,21 @@ const { upload } = require("../config/cloudinaryConfig");
 
 const router = express.Router();
 
-// ✅ Routes with image upload
 router.post(
   "/postProject",
   authMiddleware,
   adminMiddleware,
   upload.single("image"),
   createProject
-); // Upload image while creating project
+);
 router.put(
   "/updateProject/:id",
   authMiddleware,
   adminMiddleware,
   upload.single("image"),
   updateProject
-); // Upload new image while updating project
+);
 
-// Other CRUD operations
 router.get("/getProject", getProjects);
 router.get("/getprojectById/:id", getProjectById);
 router.delete(
